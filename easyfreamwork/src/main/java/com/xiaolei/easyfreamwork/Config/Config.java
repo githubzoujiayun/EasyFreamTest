320ac97cfb0e4fe732a91c86ab832b63f299550c
xiaolei <kavipyouxiang@163.com>
2017-07-21 11:08:55 +0800

    网络请求过滤器API移动至Cofig里面

--- easyfreamwork/src/main/java/com/xiaolei/easyfreamwork/Config/Config.java
+++ easyfreamwork/src/main/java/com/xiaolei/easyfreamwork/Config/Config.java
@@ -1,5 +1,8 @@
 package com.xiaolei.easyfreamwork.Config;
 
+import com.xiaolei.easyfreamwork.network.regist.Regist;
+import com.xiaolei.easyfreamwork.network.regist.RegisteTable;
+
 import java.util.Map;
 
 /**
@@ -10,31 +13,47 @@ public class Config
 {
     // 是否是调试模式
     public static boolean DEBUG = true;
-    public static Map<String,String> netHeadeMap;
+    public static Map<String, String> netHeadeMap;
 
     /**
      * 设置是否是DEBUG模式
+     *
      * @param debug
      */
     public void setDEBUG(boolean debug)
     {
         DEBUG = debug;
     }
+
     /**
      * 设置Retrofit的BaseUrl
+     *
      * @param url
      */
     public void setBaseUrl(String url)
     {
         Globals.SERVER_ADDRESS = url;
     }
-    
+
     /**
      * 设置必须的请求头
+     *
      * @param header
      */
-    public void setNetHeadeMap(Map<String,String> header)
+    public void setNetHeadeMap(Map<String, String> header)
     {
         netHeadeMap = header;
+    }
+
+    /**
+     * 注册网络请求的过滤器
+     *
+     * @param klass
+     * @param registClass
+     * @param <T>
+     */
+    public <T> void regist(Class<T> klass, Class<? extends Regist<T>> registClass)
+    {
+        RegisteTable.getInstance().regist(klass, registClass);
     }
 }
